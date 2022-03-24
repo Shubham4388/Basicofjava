7dba678bbe0478460be24dd9718068d3c2af68a2
DELL <DELL@DESKTOP-MFTV8IT>
2022-03-24 21:25:48 +0530

    ok

---
+++ Basicofjava/src/unit1/p2_task2_KeywordsInJava.java
@@ -0,0 +1,22 @@
+package unit1;
+
+public class p2_task2_KeywordsInJava {
+
+	 
+		// TODO Auto-generated method stub
+		
+		
+		protected static final double PRICE =234.90;
+		public static final double PRICE2 = 234.90;
+		
+		static final int a = 10;
+		
+		public static void main(String[] args) {
+			
+			System.out.println(PRICE);
+			
+			System.out.println(a);
+
+	}
+
+}
