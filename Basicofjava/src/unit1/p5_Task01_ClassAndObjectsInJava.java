897f2d49c2d02abe204bb7409d546d55c78f0715
DELL <DELL@DESKTOP-MFTV8IT>
2022-04-07 14:15:22 +0530

    ClassesAndObjects

---
+++ Basicofjava/src/unit1/p5_Task01_ClassAndObjectsInJava.java
@@ -0,0 +1,38 @@
+package unit1;
+
+public class p5_Task01_ClassAndObjectsInJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ABC2 obj = new ABC2();
+		obj.display2();
+		
+		ABC2.display();
+	}
+
+}
+
+class ABC2{
+	int a = 10;
+	static int b = 10;
+	
+	static void display() {
+		
+		int b = 10;
+		
+		b++;
+		
+		//System.out.println(a);
+		System.out.println(b);
+	}
+	
+	void display2() {
+		
+		System.out.println(a);
+		//As a was a local variable so we can not access it outside the method
+		//System.out.println(b);
+
+	}
+
+}
